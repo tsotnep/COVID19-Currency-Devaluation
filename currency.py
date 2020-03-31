a01701cfdd55589c3ba0c88d6decad5ccef41326
Tsotne Putkaradze <tsotnep@users.noreply.github.com>
2020-03-31 22:15:14 +0400

    plot added, with dates, separated changables

--- currency.py
+++ currency.py
@@ -8,16 +8,17 @@ from datetime import date, timedelta
 from matplotlib.ticker import MaxNLocator
 import csv
 
+#changables>>
 currencies_list=["USD","GEL","RUB","PLN","RON","UAH","TRY","GBP","RON","CAD"]
 start_date = date(2020, 1, 8)
-end_date = date(2020, 1, 10)
-
-
+end_date = date(2020, 3, 30)
+api_key = '01bb2ac5412835d13b929aea8818591b' #you might need to update API key on this line
+#<<changables
 
 s_link='http://data.fixer.io/api/' #https://fixer.io/quickstart
 s_dateKey='date='
 s_date=''
-s_access_key='?access_key=01bb2ac5412835d13b929aea8818591b' #you might need to update API key on this line
+s_access_key='?access_key='+api_key
 s_currencies_key='&symbols='
 s_currencies=','.join(currencies_list)
 n_currencies=len(currencies_list)
@@ -33,7 +34,7 @@ while check_date <= end_date:
     apilink=s_link+s_date+s_access_key+s_currencies_key+s_currencies
     req = requests.get(apilink) 
     reqjson = json.loads(req.text, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-    print(reqjson)
+    #print(reqjson)
     i=0 
     d[i].append(reqjson.rates.USD); i+=1
     d[i].append(reqjson.rates.GEL); i+=1
@@ -58,26 +59,27 @@ with open(csvRawFileName, "w", newline="") as f:
 
 #normalize
 for i in range(len(currencies_list)):
-    d[i][:] = [j / d[i][0] for j in d[i]]
-    #d[i][:] = [j / min(d[i]) for j in d[i]]
+    d[i][:] = [j / d[i][0] for j in d[i]] #current/day_before_first_corona_case
+    #d[i][:] = [j / min(d[i]) for j in d[i]] #current/minimum
 
 with open(csvNorFileName, "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(d)
 
 
-
-
 # draw
-days=np.arange(len(d[0]))
+dates_d=np.arange(start_date,end_date+timedelta(days=1))
+dates = np.datetime_as_string(dates_d)
+
 fig, ax = plt.subplots()
 
 for i in range(len(currencies_list)):
-    ax.plot(days, d[i], label=currencies_list[i])
+    ax.plot(dates, d[i], label=currencies_list[i])
+    plt.xticks(rotation=30, fontsize=5)
 
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-chartTitle='currency ratios to their min (in same range), relation with EUR price \n from '+start_date.strftime("%Y-%m-%d") + ' to '+end_date.strftime("%Y-%m-%d")
-ax.set(xlabel='days', ylabel='ratio: day[i]/min(day[:])', title=chartTitle)
+chartTitle='EUR price of DAILY_CURRENCY divided by BEFORE_CORONA_CURRENCY \n from '+start_date.strftime("%Y-%m-%d") + ' to '+end_date.strftime("%Y-%m-%d")
+ax.set(xlabel='', ylabel='ratio: currency_day[i]/currency_8.1.2020', title=chartTitle)
 
 ax.grid()
 ax.legend()
