823a6484283c1aaaa6f13a6af547ffbadba73f8a
Tsotne Putkaradze <tsotnep@users.noreply.github.com>
2020-04-01 16:11:44 +0400

    amend ffs

--- currency.py
+++ currency.py
@@ -7,84 +7,89 @@ from collections import namedtuple
 from datetime import date, timedelta
 from matplotlib.ticker import MaxNLocator
 import csv
+from itertools import cycle
 
 #changables>>
-currencies_list=["USD","GEL","RUB","PLN","RON","UAH","TRY","GBP","RON","CAD"]
+currencies_list=["USD","GEL","RUB","PLN","RON","UAH","TRY","GBP","RON","CAD","AED","AMD","AZN","IRR","MXN"] #just add any if available : https://fixer.io/symbols
 start_date = date(2020, 1, 8)
 end_date = date(2020, 3, 30)
-api_key = '01bb2ac5412835d13b929aea8818591b' #you might need to update API key on this line
+reference_date = date(2020, 1, 8) #all list of currencies will be divided by that day's currency
+api_key = '01bb2ac5412835d13b929aea8818591b' #don't be irresponsible lazy ass and get/use your API key here: https://fixer.io/quickstart
 #<<changables
 
-s_link='http://data.fixer.io/api/' #https://fixer.io/quickstart
+
+#assemble request link
+s_link='http://data.fixer.io/api/' 
 s_dateKey='date='
 s_date=''
 s_access_key='?access_key='+api_key
 s_currencies_key='&symbols='
 s_currencies=','.join(currencies_list)
-n_currencies=len(currencies_list)
 
+
+#create list of lists for currency storage
+n_currencies=len(currencies_list)
 d = [[] for reqjson in range(n_currencies)]
 
 check_date = start_date
 delta = timedelta(days=1)
 
+#retrieve data in json, store in object, and parse with for loop and put currency data in list of lists
 while check_date <= end_date:
     check_date += delta
     s_date = check_date.strftime("%Y-%m-%d")
     apilink=s_link+s_date+s_access_key+s_currencies_key+s_currencies
     req = requests.get(apilink) 
     reqjson = json.loads(req.text, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-    #print(reqjson)
-    i=0 
-    d[i].append(reqjson.rates.USD); i+=1
-    d[i].append(reqjson.rates.GEL); i+=1
-    d[i].append(reqjson.rates.RUB); i+=1
-    d[i].append(reqjson.rates.PLN); i+=1
-    d[i].append(reqjson.rates.RON); i+=1
-    d[i].append(reqjson.rates.UAH); i+=1
-    d[i].append(reqjson.rates.TRY); i+=1
-    d[i].append(reqjson.rates.GBP); i+=1
-    d[i].append(reqjson.rates.RON); i+=1
-    d[i].append(reqjson.rates.CAD); i+=1
+    ind=0
+    for curr in currencies_list:
+        d[ind].append(getattr(reqjson.rates, curr))
+        ind+=1
 
-
-
-csvRawFileName="currencies/From_"+start_date.strftime("%Y-%m-%d")+" to_"+end_date.strftime("%Y-%m-%d")+"Raw.csv"
-csvNorFileName="currencies/From_"+start_date.strftime("%Y-%m-%d")+" to_"+end_date.strftime("%Y-%m-%d")+"Normalized.csv"
-
-
+#savve raw data
+csvRawFileName="From_"+start_date.strftime("%Y-%m-%d")+" to_"+end_date.strftime("%Y-%m-%d")+"Raw.csv"
 with open(csvRawFileName, "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(d)
 
-#normalize
+#normalize - divide all currencies by the reference currency
 for i in range(len(currencies_list)):
-    d[i][:] = [j / d[i][0] for j in d[i]] #current/day_before_first_corona_case
+    d[i][:] = [j / d[i][(reference_date-start_date).days] for j in d[i]] #current/day_before_first_corona_case
     #d[i][:] = [j / min(d[i]) for j in d[i]] #current/minimum
 
+#savve normalized data
+csvNorFileName="From_"+start_date.strftime("%Y-%m-%d")+" to_"+end_date.strftime("%Y-%m-%d")+"Normalized.csv"
 with open(csvNorFileName, "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(d)
 
 
-# draw
-dates_d=np.arange(start_date,end_date+timedelta(days=1))
+# DRAW
+
+#x axis labels
+dates_d=np.arange(start_date,end_date+timedelta(days=1)) 
 dates = np.datetime_as_string(dates_d)
-
 fig, ax = plt.subplots()
-
-for i in range(len(currencies_list)):
-    ax.plot(dates, d[i], label=currencies_list[i])
-    plt.xticks(rotation=30, fontsize=5)
-
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+lines = ["-","--","-."]
+linecycler = cycle(lines)
+
+#add lists to figure
+for i in range(len(currencies_list)):
+    if (i==1) : ax.plot(dates, d[i], next(linecycler), linewidth=1, label=currencies_list[i])
+    else : ax.plot(dates, d[i], next(linecycler), linewidth=2, label=currencies_list[i])
+    plt.xticks(rotation=20, fontsize=5)
+
+#set title
 chartTitle='EUR price of DAILY_CURRENCY divided by BEFORE_CORONA_CURRENCY \n from '+start_date.strftime("%Y-%m-%d") + ' to '+end_date.strftime("%Y-%m-%d")
 ax.set(xlabel='', ylabel='ratio: currency_day[i]/currency_8.1.2020', title=chartTitle)
 
 ax.grid()
-ax.legend()
+box = ax.get_position()
+ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
+ax.legend(fontsize=9, loc='upper center', bbox_to_anchor=(0.5, -0.08),fancybox=False, shadow=False, ncol=7)
 
-
-fileName="currencies/From_"+start_date.strftime("%Y-%m-%d")+" to_"+end_date.strftime("%Y-%m-%d")+".png"
+#save to file
+fileName="From_"+start_date.strftime("%Y-%m-%d")+" to_"+end_date.strftime("%Y-%m-%d")+".png"
 fig.savefig(fileName, dpi = 300)
 #plt.show()
